5f59420f845495a3ac42540c571fda8977284b95
HInJang <wkdgydls501@gmail.com>
2025-01-15 22:45:06 +0900

    chapter 2

--- 02_end_to_end_machine_learning_project.py
+++ 02_end_to_end_machine_learning_project.py
@@ -1,15 +1,22 @@
+#파이썬 버전 확인
 import sys
 assert sys.version_info >= (3,7)
 
+#scikit-learn 버전 확인
 from packaging import version
 import sklearn
-
 assert version.parse(sklearn.__version__) >= version.parse("1.0.1")
 
+#필요한 모듈 임포트
+## Path: 파일 및 디렉터리 작업을 위해 사용
+## pandas: data analysis, 조작 라이브러리
+## tarfile:.tar or .tar.gz file 추출
+## urllib.request: URL에서 파일 다운롣,
 from pathlib import Path
 import pandas as pd
 import tarfile
 import urllib.request
+
 
 def load_housing_data():
     tarball_path = Path("datasets/housing.tgz")
@@ -20,4 +27,8 @@ def load_housing_data():
         with tarfile.open(tarball_path) as housing_tarball:
             housing_tarball.extractall(path="datasets")
     return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+#함수 호출
 housing=load_housing_data()
+
+print(housing.head())
